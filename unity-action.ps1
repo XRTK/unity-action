071af8e49cd407b7a4684b0a8bf9684a7119d7d1
Stephen Hodgson <hodgson.designs@gmail.com>
2022-11-03 16:50:58 -0400

    rework the unity action script a bit
    rename input "name" to "log-name"

--- unity-action.ps1
+++ unity-action.ps1
@@ -4,106 +4,119 @@ param(
     [String]$projectPath,
     [String]$buildTarget,
     [String]$additionalArgs,
-    [String]$name
+    [String]$name = "Unity"
 )
 
-$additionalArgs = $additionalArgs.Trim()
-$buildTargetArgs = ""
+try {
+    $buildArgs = ""
 
-if ( -not [string]::IsNullOrEmpty($buildTarget) ) {
-    $buildTargetArgs = "-buildTarget `"$buildTarget`" "
-}
+    if ( -not [string]::IsNullOrEmpty($buildTarget) ) {
+        $buildArgs += "-buildTarget `"$buildTarget`" "
+    }
 
-$logDirectory = "$projectPath/Builds/Logs"
+    if (-not [String]::IsNullOrEmpty($projectPath)) {
+        $buildArgs += "-projectPath `"$projectPath`" "
 
-if ( -not (Test-Path -Path $logDirectory)) {
-    $logDirectory = New-Item -ItemType Directory -Force -Path $logDirectory | Select-Object
-}
+        $logDirectory = "$projectPath/Builds/Logs"
 
-Write-Host "Log Directory: $logDirectory"
+        if ( -not (Test-Path -Path $logDirectory)) {
+            $logDirectory = New-Item -ItemType Directory -Force -Path $logDirectory | Select-Object
+        }
 
-$date = Get-Date -Format "yyyyMMddTHHmmss"
-$logName = "$logDirectory/$name-$date"
-$logPath = "$logName.log"
+        Write-Host "Log Directory: $logDirectory"
 
-if ( $additionalArgs -like "*runEditorTests" ) {
-    $testPath = "$logName.xml"
-    $additionalArgs += " -editorTestsResultFile `"$testPath`""
-}
+        $date = Get-Date -Format "yyyyMMddTHHmmss"
+        $logName = "$logDirectory/$name-$date"
+        $logPath = "$logName.log"
+        $buildArgs += "-logfile `"$logPath`" ";
+    }
 
-$buildArgs = "$buildTargetArgs-projectPath `"$projectPath`" -logfile `"$logPath`" $additionalArgs"
-Write-Host "::group::$editorPath $buildArgs"
+    $additionalArgs = $additionalArgs.Trim()
 
-$process = Start-Process -FilePath "$editorPath" -ArgumentList "$buildArgs" -PassThru
+    if ( $additionalArgs -like "*runEditorTests" ) {
+        $testPath = "$logName.xml"
+        $additionalArgs += " -editorTestsResultFile `"$testPath`""
+    }
 
-$ljob = Start-Job -ScriptBlock {
-    param($log)
+    $buildArgs = $buildArgs.Trim()
 
-    while ( -not (Test-Path $log -Type Leaf) ) {
+    $buildArgs += " $additionalArgs"
+    Write-Host "::group::$editorPath $buildArgs"
+
+    $process = Start-Process -FilePath "$editorPath" -ArgumentList "$buildArgs" -PassThru
+
+    $ljob = Start-Job -ScriptBlock {
+        param($log)
+
+        while ( -not (Test-Path $log -Type Leaf) ) {
+            Start-Sleep -Milliseconds 1
+        }
+
+        Get-Content "$log" -Wait
+    } -ArgumentList $logPath
+
+    $processId = $process.Id
+
+    while ( -not $process.HasExited )
+    {
+        # While waiting, Get-Content checks the file once each second
         Start-Sleep -Milliseconds 1
+        Receive-Job $ljob
+
+        if ( $null -eq (Get-Process -Id $processId -ErrorAction SilentlyContinue) )
+        {
+            Write-Host "Unity process has ended unexpectedly..."
+            break
+        }
     }
 
-    Get-Content "$log" -Wait
-} -ArgumentList $logPath
+    Write-Host "Unity Process $processId Complete!"
 
-$processId = $process.Id
+    # Wait for the last of the log information to be written
+    $fileLocked = $true
+    $timeout = New-TimeSpan -Seconds 10
+    $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
-while ( -not $process.HasExited )
-{
-    # While waiting, Get-Content checks the file once each second
-    Start-Sleep -Milliseconds 1
+    do {
+        try
+        {
+            $file = Convert-Path $logPath
+            $fileStream = [System.IO.File]::Open($file,'Open','Write')
+            $fileStream.Close()
+            $fileStream.Dispose()
+            $fileLocked = $false
+        }
+        catch
+        {
+            $fileLocked = $true
+        }
+
+        if ( $stopwatch.elapsed -lt $timeout )
+        {
+        if ( (-not $global:PSVersionTable.Platform) -or ($global:PSVersionTable.Platform -eq "Win32NT") ) {
+            Write-Host "Attempting to cleanup orphaned processes..."
+            $procsWithParent = Get-CimInstance -ClassName "win32_process" | Select-Object ProcessId,ParentProcessId
+            $orphaned = $procsWithParent | Where-Object -Property ParentProcessId -NotIn $procsWithParent.ProcessId
+            $procs = Get-Process -IncludeUserName | Where-Object -Property Id -In $orphaned.ProcessId | Where-Object { $_.UserName -match $env:username }
+            $procs | ForEach-Object { Stop-Process -Id $_.Id -ErrorAction SilentlyContinue }
+        }
+        }
+
+        Start-Sleep -Milliseconds 1
+    } while ( $fileLocked )
+
+    Write-Host "End of log stream"
+    Write-Host "Cleaning up jobs..."
+
+    # Clean up job
     Receive-Job $ljob
+    Stop-Job $ljob
+    Remove-Job $ljob
 
-    if ( $null -eq (Get-Process -Id $processId -ErrorAction SilentlyContinue) )
-    {
-        Write-Host "Unity process has ended unexpectedly..."
-        break
-    }
+    Write-Host "::endgroup::"
+
+    exit $process.ExitCode
 }
-
-Write-Host "Unity Process $processId Complete!"
-
-# Wait for the last of the log information to be written
-$fileLocked = $true
-$timeout = New-TimeSpan -Seconds 10
-$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
-
-do {
-    try
-    {
-        $file = Convert-Path $logPath
-        $fileStream = [System.IO.File]::Open($file,'Open','Write')
-        $fileStream.Close()
-        $fileStream.Dispose()
-        $fileLocked = $false
-    }
-    catch
-    {
-        $fileLocked = $true
-    }
-
-    if ( $stopwatch.elapsed -lt $timeout )
-    {
-    if ( (-not $global:PSVersionTable.Platform) -or ($global:PSVersionTable.Platform -eq "Win32NT") ) {
-        Write-Host "Attempting to cleanup orphaned processes..."
-        $procsWithParent = Get-CimInstance -ClassName "win32_process" | Select-Object ProcessId,ParentProcessId
-        $orphaned = $procsWithParent | Where-Object -Property ParentProcessId -NotIn $procsWithParent.ProcessId
-        $procs = Get-Process -IncludeUserName | Where-Object -Property Id -In $orphaned.ProcessId | Where-Object { $_.UserName -match $env:username }
-        $procs | ForEach-Object { Stop-Process -Id $_.Id -ErrorAction SilentlyContinue }
-    }
-    }
-
-    Start-Sleep -Milliseconds 1
-} while ( $fileLocked )
-
-Write-Host "End of log stream"
-Write-Host "Cleaning up jobs..."
-
-# Clean up job
-Receive-Job $ljob
-Stop-Job $ljob
-Remove-Job $ljob
-
-Write-Host "::endgroup::"
-
-exit $process.ExitCode
+catch {
+    exit 1
+}
