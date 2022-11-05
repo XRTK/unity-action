ee6d42974e5f212ee95a9e881fd6884e0c10ded5
Stephen Hodgson <hodgson.designs@gmail.com>
2022-11-05 13:26:20 -0700

    dist action build

---
+++ dist/index.js
@@ -0,0 +1,140 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 27:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 118:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/exec");
+
+
+/***/ }),
+
+/***/ 889:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/io");
+
+
+/***/ }),
+
+/***/ 17:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("path");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(27);
+const exec = __nccwpck_require__(118);
+const io = __nccwpck_require__(889);
+const path = __nccwpck_require__(17);
+
+const main = async () => {
+    try {
+        var args = "";
+        var editorPath = core.getInput('editor-path');
+
+        if (!editorPath) {
+            throw Error("Missing editor-path input");
+        }
+
+        args += `-editorPath "${editorPath}" `;
+
+        var projectPath = core.getInput('project-path');
+
+        if (!projectPath) {
+            throw Error("Missing project-path input");
+        }
+
+        args += `-projectPath "${projectPath}" `;
+
+        var buildTarget = core.getInput('build-target');
+
+        if (buildTarget) {
+            args += `-buildTarget ${buildTarget} `;
+        }
+
+        var additionalArgs = core.getInput('args');
+
+        if (args) {
+            args += `-additionalArgs "${additionalArgs}" `;
+        }
+
+        var logName = core.getInput('log-name');
+
+        if (!logName) {
+            throw Error("Missing log-name input");
+        }
+
+        args += `-logName ${logName}`
+
+        var pwsh = await io.which("pwsh", true);
+
+        var unity_action = __nccwpck_require__.ab + "unity-action.ps1";
+        var exitCode = await exec.exec(`"${pwsh}" -Command`, `${unity_action} ${args}`);
+
+        if (exitCode != 0) {
+            throw Error(`Unity Action Failed! exitCode: ${exitCode}`)
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+// Call the main function to run the action
+main();
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
---
+++ dist/unity-action.ps1
@@ -0,0 +1,119 @@
+# Unity Action
+param(
+    [String]$editorPath,
+    [String]$projectPath,
+    [String]$buildTarget,
+    [String]$additionalArgs,
+    [String]$logName = "Unity"
+)
+
+try {
+    $buildArgs = ""
+
+    if ( -not [string]::IsNullOrEmpty($buildTarget) ) {
+        $buildArgs += "-buildTarget `"$buildTarget`" "
+    }
+
+    if (-not [String]::IsNullOrEmpty($projectPath)) {
+        $buildArgs += "-projectPath `"$projectPath`" "
+
+        $logDirectory = "$projectPath/Builds/Logs"
+
+        if ( -not (Test-Path -Path $logDirectory)) {
+            $logDirectory = New-Item -ItemType Directory -Force -Path $logDirectory | Select-Object
+        }
+
+        Write-Host "Log Directory: $logDirectory"
+
+        $date = Get-Date -Format "yyyyMMddTHHmmss"
+        $fullLogName = "$logDirectory/$logName-$date"
+        $logPath = "$fullLogName.log"
+        $buildArgs += "-logfile `"$logPath`" ";
+    }
+
+    $additionalArgs = $additionalArgs.Trim()
+
+    if ( $additionalArgs -like "*runEditorTests" ) {
+        $testPath = "$fullLogName.xml"
+        $additionalArgs += " -editorTestsResultFile `"$testPath`""
+    }
+
+    $buildArgs = $buildArgs.Trim()
+
+    $buildArgs += " $additionalArgs"
+
+    $process = Start-Process -FilePath "$editorPath" -ArgumentList "$buildArgs" -PassThru
+
+    $ljob = Start-Job -ScriptBlock {
+        param($log)
+
+        while ( -not (Test-Path $log -Type Leaf) ) {
+            Start-Sleep -Milliseconds 1
+        }
+
+        Get-Content "$log" -Wait
+    } -ArgumentList $logPath
+
+    $processId = $process.Id
+
+    while ( -not $process.HasExited )
+    {
+        # While waiting, Get-Content checks the file once each second
+        Start-Sleep -Milliseconds 1
+        Receive-Job $ljob
+
+        if ( $null -eq (Get-Process -Id $processId -ErrorAction SilentlyContinue) )
+        {
+            Write-Host "Unity process has ended unexpectedly..."
+            break
+        }
+    }
+
+    Write-Host "Unity Process $processId Complete!"
+
+    # Wait for the last of the log information to be written
+    $fileLocked = $true
+    $timeout = New-TimeSpan -Seconds 10
+    $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+
+    do {
+        try
+        {
+            $file = Convert-Path $logPath
+            $fileStream = [System.IO.File]::Open($file,'Open','Write')
+            $fileStream.Close()
+            $fileStream.Dispose()
+            $fileLocked = $false
+        }
+        catch
+        {
+            $fileLocked = $true
+        }
+
+        if ( $stopwatch.elapsed -lt $timeout )
+        {
+            if ( (-not $global:PSVersionTable.Platform) -or ($global:PSVersionTable.Platform -eq "Win32NT") ) {
+                Write-Host "Attempting to cleanup orphaned processes..."
+                $procsWithParent = Get-CimInstance -ClassName "win32_process" | Select-Object ProcessId,ParentProcessId
+                $orphaned = $procsWithParent | Where-Object -Property ParentProcessId -NotIn $procsWithParent.ProcessId
+                $procs = Get-Process -IncludeUserName | Where-Object -Property Id -In $orphaned.ProcessId | Where-Object { $_.UserName -match $env:username }
+                $procs | ForEach-Object { Stop-Process -Id $_.Id -ErrorAction SilentlyContinue }
+            }
+        }
+
+        Start-Sleep -Milliseconds 1
+    } while ( $fileLocked )
+
+    Write-Host "End of log stream"
+    Write-Host "Cleaning up jobs..."
+
+    # Clean up job
+    Receive-Job $ljob
+    Stop-Job $ljob
+    Remove-Job $ljob
+
+    exit $process.ExitCode
+}
+catch {
+    exit 1
+}
